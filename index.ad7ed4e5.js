d0a8fc0c52e584fa64be23ca4c6db79cff26f9cc
purplefade <tarovykanton@gmail.com>
2024-01-18 14:13:31 +0200

    make build

---
+++ index.ad7ed4e5.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-console */function e(e,t){return new Promise(function(n){e.addEventListener(t,function(){n("It was\n        ".concat(t," on the element: ").concat(e.nodeName,", id: ").concat(e.id))})})}var t=document.querySelector("body"),n=function(e){var n=document.createElement("div");n.classList.add("message"),n.textContent=e,t.appendChild(n)},c=document.getElementById("login"),d=document.getElementById("password"),o=document.getElementById("submit");e(c,"click").then(n),e(d,"click").then(n),e(o,"click").then(n),e(c,"input").then(n),e(d,"input").then(n),e(c,"blur").then(n),e(d,"blur").then(n),e(o,"blur").then(n);//# sourceMappingURL=index.ad7ed4e5.js.map
+
+//# sourceMappingURL=index.ad7ed4e5.js.map
